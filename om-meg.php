edaf75ee7cb2f2a0b772cfd38fee47ce8e98e8d1
Jakob <jakob@mandelid.net>
2026-03-25 13:39:07 +0100

    lagt til spotify data

--- om-meg.php
+++ om-meg.php
@@ -13,7 +13,7 @@
         <h1>Om meg og hvor du kan finne meg</h1>
         <section class="container">
             <section class="yt-header">
-                <h2>Youtube</h2>
+                <h2>Youtube og Spotify</h2>
             </section>
             <section class="yt-content">
                 <h3>Min YouTube kanal</h3>
@@ -34,6 +34,28 @@
                             <section class="short3">
                             <p>Denne har 5 tusen seere</p>
                         <iframe width="350" height="710" src="https://www.youtube.com/embed/CPr4HqNMD8M" title="8-9 Ace Short  #valorant #valorantclips #ace" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </section>
+                </section>
+            </section>
+            <section class="spotify">
+                <h2>Spotify</h2>
+                <h3>Noen sanger jeg liker*</h3>
+                <p>*for nå har jeg tatt mine top spilte sanger i 2025</p>
+                <section class="container-spotify-history">
+                    <section class="song1">
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/190jyVPHYjAqEaOGmMzdyk?utm_source=generator" width="100%" height="264" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    </section>
+                    <section class="song2">
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/0w5Bdu51Ka25Pf3hojsKHh?utm_source=generator" width="100%" height="264" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    </section>
+                    <section class="song3">
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/3J5y9v6vK5ginTnMroJ1Nn?utm_source=generator" width="100%" height="264" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    </section>
+                    <section class="song4">
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/2V4bv1fNWfTcyRJKmej6Sj?utm_source=generator" width="100%" height="264" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    </section>
+                    <section class="song5">
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/7MmG8p0F9N3C4AXdK6o6Eb?utm_source=generator" width="100%" height="264" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                     </section>
                 </section>
             </section>
